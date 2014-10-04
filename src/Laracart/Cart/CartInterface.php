b6f3189e6f3c5494f92fc418ab91dea224d2868c
Marcelo Silva <marcelosilva.developer@gmail.com>
2014-10-04 05:25:30 -0300

    Refatorando interface do carrinho

--- src/Laracart/Cart/CartInterface.php
+++ src/Laracart/Cart/CartInterface.php
@@ -6,15 +6,27 @@ interface CartInterface
 	/**
 	 * Get all products
 	 */
-	public function all();
+	public function getProducts();
+
+	/**
+	 * Get product by id
+	 *
+	 * @param integer $id
+	 */
+	public function getProductById($id);
 
 	/**
 	 * Add product by key
+	 * 
+	 * @param array $product
+	 * @param integer $quantity
 	 */
-	public function add($product);
+	public function addProduct($product, $quantity = null);
 
 	/**
 	 * Delete product by key
+	 *
+	 * @param integer $id
 	 */
-	public function delete($product);
+	public function deleteProduct($id);
 }
