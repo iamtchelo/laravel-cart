9417277f09d8f4340037eecaec1bb51722ce190d
Marcelo Silva <marcelosilva.developer@gmail.com>
2014-10-04 05:28:21 -0300

    Refatoração do carrinho

--- src/Laracart/Cart/Cart.php
+++ src/Laracart/Cart/Cart.php
@@ -1,38 +1,106 @@
 <?php
 namespace Laracart\Cart;
 
-class Cart implements CartInterface
+class Cart implements CartInterface, \Countable
 {
+	/**
+	 * @var string
+	 */
+	private $prefix = 'products';
 
+	/**
+	 * @var integer
+	 */
+	private $totalProduct = 0;
 
-	public function all()
+	/**
+	 * @var float
+	 */
+	private $totalPrice;
+
+	public function __construct()
 	{
-
+		$_SESSION[$this->prefix] = array();
 	}
 
 	/**
-	* Add to cart
-	*/
-	public function add($product)
+	 * Get all products
+	 */
+	public function getProducts()
 	{
+		if (count($_SESSION[$this->prefix]) >= 1) {
+			return $_SESSION[$this->prefix];
+		}
+	}
 
-		if(count($_SESSION['products'][$product['id']])){
-		  throw new Exception("O produto já existe");
+	/**
+	 * Get product by id
+	 *
+	 * @param integer $id
+	 */
+	public function getProductById($id)
+	{
+		if ($this->has($id)) {
+			return $_SESSION[$this->prefix][$id];
+		}
+	}
+
+	/**
+	 * Add product by key
+	 * 
+	 * @param array $product
+	 * @param integer $quantity
+	 */
+	public function addProduct($product, $quantity = null)
+	{	
+		if (!is_array($product) || $product == null) {
+			throw new \InvalidArgumentException('Invalid product');
 		}
 
-		$_SESSION['products'][$product['id']] = $product ;
-
+		$quantity = ($quantity != null) ? $quantity : (int) 1;
+		if ($this->has($product['id'])) {
+			$product = $this->getProductById($product['id']);
+			$product['quantity'] += $quantity;
+			$_SESSION[$this->prefix][$product['id']] = $product;
+		} else {
+			$product['quantity'] = $quantity;
+			$_SESSION[$this->prefix][$product['id']] = $product;
+		}
 	}
 
 	
 	/**
-	* Delete to cart
-	*/
-	public function delete($product)
-	{
-
-				
+	 * Delete product by key
+	 *
+	 * @param integer $id
+	 */
+	public function deleteProduct($id)
+	{	
+		if ($this->has($id)) {
+			unset($_SESSION[$this->prefix][$id]);
+		}	
 	}
 
+	/**
+	 * If has product
+	 *
+	 * @param integer $id
+	 * @return TRUE if has or FALSE
+	 */
+	private function has($id)
+	{
+		return (isset($_SESSION[$this->prefix][$id])) ? true : false;
+	}
 
+	/**
+	 * Count all products
+	 */
+	public function count()
+	{
+		foreach ($_SESSION[$this->prefix] as $product) {
+			$this->totalProduct += $product['quantity'];
+		}
+
+		return $this->totalProduct;
+	}
 }
